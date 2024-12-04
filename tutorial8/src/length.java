ab9433c47264aa3387e2b82432a41273e15e4711
HimaraJoseph <himara.20231617@iit.ac.lk>
2024-12-04 12:24:51 +0530

    Delete tutorial8 directory

--- tutorial8/src/length.java
+++
@@ -1,9 +0,0 @@
-public class length {
-    public static void main(String[] args) {
-        String a = " Hello, World! ";
-        System.out.println("This word contains " + a.length() + " characters");
-    }
-}
-
-//substring - a.substring();
-//a.toLowerCase
--- tutorial8/src/retrieve.java
+++
@@ -1,8 +0,0 @@
-public class retrieve {
-    public static void main(String[] args) {
-        String ret = "Hello, World!";
-        System.out.println("Modified string is: "+ ret.replace('r','a'));
-
-        System.out.println(ret.replaceFirst("World", "Himara"));
-    }
-}
